b18cf084eea10ea5570991967445fd1774aee4af
Oladipupo Ishola <olaishola@hotmail.co.uk>
2024-07-29 21:14:22 +0100

    feat(app): Initial commit
    Dockerize the app using a prod build with nginx

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("Should renders Something random with some degrees of randomness", () => {
+  render(<App />);
+  const pElemnt = screen.getByText(/Something random with some degrees of randomness/i);
+  expect(pElemnt).toBeInTheDocument()
+})
+
+test("renders without crashing", () => {
+  const div = document.createElement('div');
+  render(<App />, div)
+  expect(div).toBeEmptyDOMElement()
+})
+
+
